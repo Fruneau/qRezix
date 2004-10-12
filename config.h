e465138003181e4a689a3b43a32f4e8f0734b519
pico <pico@30024bfc-89fa-0310-95ff-ee513f6fc940>
2004-10-12 18:02:48 +0000

    This commit was generated by cvs2svn to compensate for changes in r378, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn://localhost/qrezix/trunk@379 30024bfc-89fa-0310-95ff-ee513f6fc940

--- config.h
+++ config.h
@@ -117,7 +117,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.5.1"
+#define VERSION "1.6"
 
 /*
  * jpeg.h needs HAVE_BOOLEAN, when the system uses boolean in system
