54cc1dcb734ef204e6547a1d854d42cff40b783f
fruneau <fruneau@30024bfc-89fa-0310-95ff-ee513f6fc940>
2005-12-09 19:18:20 +0000

    Manquait un en-tÃªte GPL :/
    git-svn-id: svn://localhost/qrezix/trunk@1173 30024bfc-89fa-0310-95ff-ee513f6fc940

--- core/rzxproperty.cpp
+++ core/rzxproperty.cpp
@@ -4,6 +4,15 @@
 copyright            : (C) 2002 by Benoit Casoetto
 email                : benoit.casoetto@m4x.org
 ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include <QPushButton>
 #include <QToolButton>
 #include <QToolBox>
