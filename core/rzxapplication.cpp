d117535e64827595df4f88d26f24fbdf3774eb1b
fruneau <fruneau@30024bfc-89fa-0310-95ff-ee513f6fc940>
2006-01-08 22:07:20 +0000

    Je psychote !!!
    git-svn-id: svn://localhost/qrezix/trunk@1329 30024bfc-89fa-0310-95ff-ee513f6fc940

--- core/rzxapplication.cpp
+++ core/rzxapplication.cpp
@@ -27,6 +27,11 @@
 #include <RzxProperty>
 #include <RzxIntro>
 
+#ifdef Q_OS_MAC
+#	include <Carbon/Carbon.h>
+#endif
+
+
 #ifdef RZX_MAINUI_BUILTIN
 #	include "../modules/mainui/rzxui.h"
 #endif
@@ -149,6 +154,17 @@ bool RzxApplication::loadCore()
 	//Chargement des configs
 	setSettings(new RzxConfig());
 	setWindowIcon(RzxIconCollection::qRezixIcon());
+#ifdef Q_OS_MAC
+	const QPixmap pixmap = RzxIconCollection::qRezixIcon();
+  	if(pixmap.isNull()) {
+		RestoreApplicationDockTileImage();
+        } else {
+	        QPixmap scaled_pixmap = pixmap.scaled(128, 128);
+	        CGImageRef ir = (CGImageRef)scaled_pixmap.macCGHandle();
+	        SetApplicationDockTileImage(ir);
+	}
+#endif
+  
 
 	//Initialisation de l'objet représentant localhost
 	RzxComputer::localhost();
@@ -493,7 +509,6 @@ void RzxApplication::displayHelp()
 }
 
 #ifdef Q_OS_MAC
-#include <Carbon/Carbon.h>
 bool RzxApplication::macEventFilter( EventHandlerCallRef caller, EventRef event )
 {
 	if(GetEventClass(event) == kEventClassApplication && GetEventKind(event) == kEventAppActivated && mainui)
