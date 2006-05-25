4fe9ae3d5a8dd8fdabe4be5ee4c8a766858d443e
fruneau <fruneau@30024bfc-89fa-0310-95ff-ee513f6fc940>
2006-05-25 08:14:20 +0000

    Traductions pour les notifications
    git-svn-id: svn://localhost/qrezix/trunk@1702 30024bfc-89fa-0310-95ff-ee513f6fc940

--- resources/translations/notifier_fr.ts
+++ resources/translations/notifier_fr.ts
@@ -24,6 +24,22 @@
         <source>Old style - like qRezix 1.6</source>
         <translation>Style &apos;Vieux&apos; - comme qRezix 1.6</translation>
     </message>
+    <message>
+        <source>This is an example of received text</source>
+        <translation>Exemple de texte reçu par chat...</translation>
+    </message>
+    <message>
+        <source>No notifications for connection status</source>
+        <translation>Pas de notification de changement d&apos;état</translation>
+    </message>
+    <message>
+        <source>Growl - Use Growl notification system</source>
+        <translation>Growl - Utilise Growl comme système de notification</translation>
+    </message>
+    <message>
+        <source>No notifications for chat</source>
+        <translation>Pas de notifications pour le chat</translation>
+    </message>
 </context>
 <context>
     <name>RzxNotifierProp</name>
@@ -33,7 +49,7 @@
     </message>
     <message>
         <source>When ?</source>
-        <translation>Quand ?</translation>
+        <translation type="obsolete">Quand ?</translation>
     </message>
     <message>
         <source>Notify when favorite go away</source>
@@ -53,15 +69,15 @@
     </message>
     <message>
         <source>What ?</source>
-        <translation>Comment ?</translation>
+        <translation type="obsolete">Comment ?</translation>
     </message>
     <message>
         <source>&lt;h3&gt;Notification&lt;/h3&gt;&lt;p&gt;Check it if you want a small nofitication windows to be displayed when a favorite connects, disconnects, or change is away-state&lt;/p&gt;</source>
-        <translation>&lt;h3&gt;Notification&lt;/h3&gt;&lt;p&gt;Si vous cochez, une fenêtre de notification s&apos;affichera chaque fois qu&apos;un favoris se connecte, se déconnecte ou change d&apos;état&lt;/p&gt;</translation>
+        <translation type="obsolete">&lt;h3&gt;Notification&lt;/h3&gt;&lt;p&gt;Si vous cochez, une fenêtre de notification s&apos;affichera chaque fois qu&apos;un favoris se connecte, se déconnecte ou change d&apos;état&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Show a notification window on favorite&apos;s state modification</source>
-        <translation>Afficher les fenêtres de notification des favoris</translation>
+        <translation type="obsolete">Afficher les fenêtres de notification des favoris</translation>
     </message>
     <message>
         <source>&lt;h3&gt;Beep on connection&lt;/h3&gt;&lt;p&gt;Select the sound to play when a favorite get connected&lt;/p&gt;</source>
@@ -81,11 +97,90 @@
     </message>
     <message>
         <source>Notification windows style:</source>
-        <translation>Style des fenêtres de notification :</translation>
+        <translation type="obsolete">Style des fenêtres de notification :</translation>
     </message>
     <message>
         <source>Test</source>
         <translation>Tester</translation>
+    </message>
+    <message>
+        <source>Chat</source>
+        <translation>Discussions</translation>
+    </message>
+    <message>
+        <source>Style:</source>
+        <translation>Style :</translation>
+    </message>
+    <message>
+        <source>No chat notification when qRezix is the foreground application</source>
+        <translation>Pas de notification de discussions lorsque qRezix est au premier-plan</translation>
+    </message>
+    <message>
+        <source>Notify when receive a chat message</source>
+        <translation>Notifier la réception des messages</translation>
+    </message>
+    <message>
+        <source>Global</source>
+        <translation>Global</translation>
+    </message>
+    <message>
+        <source>Connection state</source>
+        <translation>Etat de connexion</translation>
+    </message>
+</context>
+<context>
+    <name>RzxNotifierPropUI</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished">Formulaire</translation>
+    </message>
+    <message>
+        <source>When ?</source>
+        <translation type="unfinished">Quand ?</translation>
+    </message>
+    <message>
+        <source>Notify when favorite go away</source>
+        <translation type="unfinished">Notifier lorsqu&apos;un favori est marqué absent</translation>
+    </message>
+    <message>
+        <source>No notification when I am away</source>
+        <translation type="unfinished">Pas de notification lorsque je suis marqué absent</translation>
+    </message>
+    <message>
+        <source>Notify favorite disconnection</source>
+        <translation type="unfinished">Notifier la déconnexion des favoris</translation>
+    </message>
+    <message>
+        <source>Notify when favorite arrive or come back</source>
+        <translation type="unfinished">Notifier l&apos;arrivée et le retour des favoris</translation>
+    </message>
+    <message>
+        <source>What ?</source>
+        <translation type="unfinished">Comment ?</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Notification&lt;/h3&gt;&lt;p&gt;Check it if you want a small nofitication windows to be displayed when a favorite connects, disconnects, or change is away-state&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;h3&gt;Notification&lt;/h3&gt;&lt;p&gt;Si vous cochez, une fenêtre de notification s&apos;affichera chaque fois qu&apos;un favoris se connecte, se déconnecte ou change d&apos;état&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Show a notification window on favorite&apos;s state modification</source>
+        <translation type="unfinished">Afficher les fenêtres de notification des favoris</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Beep on connection&lt;/h3&gt;&lt;p&gt;Select the sound to play when a favorite get connected&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;h3&gt;Bip à la connexion&lt;/h3&gt;&lt;p&gt;Choisissez le son à jouer à la connexion des favoris&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished">...</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Beep on connection&lt;/h3&gt;&lt;p&gt;Check this if you want qRezix to play a sound when a favorite get connected&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;h3&gt;Bip à la connexion&lt;/h3&gt;&lt;p&gt;Si vous cochez, un son sera joué à chaque connexion de favoris&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Beep on connection of one of your favorites</source>
+        <translation type="unfinished">Bip à la connexion des favoris</translation>
     </message>
 </context>
 <context>
@@ -101,6 +196,18 @@
     <message>
         <source>is now disconnected</source>
         <translation>s&apos;est déconnecté</translation>
+    </message>
+    <message>
+        <source>says:</source>
+        <translation>dit :</translation>
+    </message>
+    <message>
+        <source>qRezix favorite change...</source>
+        <translation>Changement d&apos;état sur qRezix...</translation>
+    </message>
+    <message>
+        <source>says on qRezix:</source>
+        <translation>dit sur qRezix :</translation>
     </message>
 </context>
 </TS>
