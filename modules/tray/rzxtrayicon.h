fc719e8b6a16eaf4b6f885e454c9addc262a382f
gui <gui@30024bfc-89fa-0310-95ff-ee513f6fc940>
2007-03-08 22:27:18 +0000

    Port de la trayicon en Qt 4 (QSystemTrayIcon)  * resout le bug du toolTip à "O connectés" sous windows
    Il faut vérifier que ca passe bien à linux et Mac
    
    git-svn-id: svn://localhost/qrezix/trunk@1801 30024bfc-89fa-0310-95ff-ee513f6fc940

--- modules/tray/rzxtrayicon.h
+++ modules/tray/rzxtrayicon.h
@@ -1,6 +1,7 @@
 /*
- * trayicon.h - system-independent trayicon class (adapted from Qt example)
- * Copyright (C) 2003  Justin Karneges
+ * trayicon.h - trayicon class
+ * Florent Bruneau, Copyright (C) 2004-2005 Binet Réseau
+ * Ported to Qt 4 by Guillaume Bandet, 2007
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,11 +17,10 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Adaptation to qRezix by Florent Bruneau, Copyright (C) 2004-2005 Binet Réseau
  */
 
-#ifndef CS_TRAYICON_H
-#define CS_TRAYICON_H
+#ifndef RZXTRAYICON_H
+#define RZXTRAYICON_H
 
 #undef RZX_BUILTIN
 #undef RZX_PLUGIN
@@ -30,13 +30,8 @@
 #	define RZX_PLUGIN
 #endif
 
-#include <QObject>
-#include <QImage>
+#include <QSystemTrayIcon>
 #include <QMenu>
-#include <QPoint>
-#include <QPixmap>
-#include <QMouseEvent>
-#include <QEvent>
 
 #include <RzxModule>
 
@@ -57,40 +52,17 @@ public:
 	RzxTrayIcon();
 	~RzxTrayIcon();
 
-	// use WindowMaker dock mode.  ignored on non-X11 platforms
-	void setWMDock(bool use) { v_isWMDock = use; }
-	bool isWMDock() { return v_isWMDock; }
-
 	QPixmap trayIcon() const;
 	QString toolTip() const;
-
-	void gotCloseEvent();
-	virtual bool isInitialised() const;
-
-	QPoint getPos();
+	virtual bool isInitialised() const;	//TODO a voir
 
 public slots:
 	void changeTrayIcon();
+	void buildMenu();
 	void setTrayIcon( const QPixmap &icon );
 	void setToolTip( const QString &tip );
 
-	void buildMenu();
-	void setVisible(bool yes);
-	virtual void show();
-	virtual void hide();
-
-signals:
-	void clicked(const QPoint&);
-	void doubleClicked(const QPoint&);
-	void closed();
-
 protected:
-	bool event( QEvent * );
-	virtual void mouseMoveEvent( QMouseEvent *e );
-	virtual void mousePressEvent( QMouseEvent *e );
-	virtual void mouseReleaseEvent( QMouseEvent *e );
-	virtual void mouseDoubleClickEvent( QMouseEvent *e );
-
 	QString titleFromSplit(int j, QString splitPoints);
 	int     calculerSplit(QString& splitPoints, QList<RzxComputer*> list);
 	void    subMenu(QMenu& mname, Rzx::Icon micon, const char * slot, QList<RzxComputer*> list, QList<RzxComputer*> fav);
@@ -103,21 +75,10 @@ private:
 	QMenu news;
 	QMenu samba;
 	QMenu mail;
-	QPixmap pm;
-	QString tip;
-	bool v_isWMDock;
+	QSystemTrayIcon* tray;
 
 // system-dependant part
 public:
-	class RzxTrayIconPrivate;
-private:
-	RzxTrayIconPrivate *d;
-	void sysInstall();
-	void sysRemove();
-	void sysUpdateIcon();
-	void sysUpdateToolTip();
-
-	friend class RzxTrayIconPrivate;
 
 #ifndef Q_OS_MAC
 private:
@@ -136,10 +97,11 @@ public slots:
 	virtual void propClose();
 
 protected slots:
+	void trayActivated(QSystemTrayIcon::ActivationReason reason);
 	void translate();
 	void updateBackground();
 	void selectColor();
 #endif
 };
 
-#endif // CS_TRAYICON_H
+#endif
